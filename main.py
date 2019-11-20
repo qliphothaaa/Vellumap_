06e6a8ff746a2e34e283a63a29d505184cc7cbb6
qliphoth <paradox.qliphoth@gmail.com>
2019-11-20 14:19:51 +0700

    redo gitignore

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import *
-
-from view.vellumap_window import VellumapWindow
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    wnd = VellumapWindow()
-    sys.exit(app.exec_())
--- readme.txt
+++
@@ -1,7 +0,0 @@
-The Vellumap is a custom map system.
-Build by:
-
-Python      3.7.4
-PyQt5       5.12.0
-SQLite      3.24.0
-
